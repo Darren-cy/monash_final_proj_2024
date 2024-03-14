76a661bdd8a10d4f4c80c388ae9104182a31e8ab
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-03-14 20:08:46 +1100

    (insecure) proof of concept for login working

--- dms/__init__.py
+++ dms/__init__.py
@@ -1,18 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
+
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'dms.sqlite')
+    )
     
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
     
+    # Create the app directory if it doesn't already exist
+    try:
+        os.makedirs(app.instance_path)
+    except FileExistsError:
+        pass
+    
     @app.route('/')
-    def hello():
-        return "Hello, world!"
+    def index():
+        return render_template('dashboard.html')
+    
+    from . import db
+    db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
     
     return app
 
