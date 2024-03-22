bd2fc54ea2b395f6ce24d8a82d57abdbf254b3ed
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-03-22 17:37:21 +1100

    Added model for user

--- dms/db.py
+++ dms/db.py
@@ -1,1 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    """Database base class"""
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column("user_id", primary_key=True)
+    name: Mapped[str] = mapped_column("user_name", nullable=False)
+    email: Mapped[str] = mapped_column("user_email", nullable=False)
+    password: Mapped[str] = mapped_column("user_password", nullable=False)
 
