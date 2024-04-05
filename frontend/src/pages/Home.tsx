6777b626c5f93bd4a943b041a5fee81573f1a5e4
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-04-05 21:19:18 +1100

    Add getData function to fetch protected data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,9 +2,20 @@ import { useState } from "react";
 import api from "../api";
 
 const Home = () => {
+  const [message, setMessage] = useState("");
+  const getData = async () => {
+    try {
+      const response = await api.get("/api/v1.0/protected");
+      setMessage(response.data.message);
+      console.log(response.data);
+    } catch (error) {
+      setMessage("An error occurred");
+    }
+  }
   return (
     <div>
         <h1>Welcome to document processing app</h1>
+        <button onClick={getData}>Get Data</button>
     </div>
   )
 }
