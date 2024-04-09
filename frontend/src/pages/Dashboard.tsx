be94e972824f0e6790c5b465c75ec85f738b1606
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-04-09 15:09:43 +1000

    Add link to Document Processing page in Dashboard component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <div>Dashboard</div>
+      <Link to="/document-processing">Document Processing</Link>
+    </div>
   )
 }
 
