9790aee22269ef467490dc1ab0d0be98faaad5fe
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-04-16 15:56:24 +1000

    Add NavigationBar component to Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,13 @@
 import Form from "../components/Form";
+import NavigationBar from "../components/NavigationBar";
 
 const Login = () => {
   return (
-    <Form route="api/v1.0/login" method="login" />
-  )
-}
+    <div>
+      <NavigationBar />
+      <Form route="api/v1.0/login" method="login" />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
