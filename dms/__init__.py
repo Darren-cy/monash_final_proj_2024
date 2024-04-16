6a60b8e2f4857736d6a373d77d20c16255d81858
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-04-16 15:56:36 +1000

    Update Flask app to handle file uploads

--- dms/__init__.py
+++ dms/__init__.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, g, current_app
+from flask import Flask, render_template, send_from_directory, request
 from flask_cors import CORS
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+import os
 
 # Load the environment variables
 load_dotenv()
 # Create the Flask app
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True,
+            static_folder='static', template_folder='templates')
 # Enable CORS
 CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
 
@@ -33,6 +35,7 @@ with app.app_context():
     db.session.commit()
 # Register the index route
 
+
 @app.route('/')
 def index():
     return render_template('dashboard.html')
@@ -42,3 +45,19 @@ from . import auth, api
 app.register_blueprint(auth.bp)
 app.register_blueprint(api.bp)
 
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        file = request.files['file']
+        # Create a folder named uploads in the static folder
+        os.makedirs('dms/static/uploads/', exist_ok=True)
+        file.save('dms/static/uploads/' + file.filename)
+        return 'File uploaded successfully', 200
+    except Exception as e:
+        return str(e), 400
