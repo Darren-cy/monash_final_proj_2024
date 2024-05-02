b3b994916bf3e131f8f13efc5845a4f3fa1e1460
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-05-02 12:32:39 +1000

    Implemented document endpoints for get and post

--- dms/api/document.py
+++ dms/api/document.py
@@ -1,23 +1,35 @@
+import os.path
+from datetime import datetime, timezone
+from uuid import uuid4
 from flask_restful import Resource, fields, marshal_with  # type: ignore
+from flask_restful.reqparse import RequestParser  # type: ignore
 from dms.models import Document
 from sqlalchemy import Select
-from flask import current_app
+from sqlalchemy.exc import IntegrityError
+from flask import current_app, abort
+import werkzeug.datastructures
+from mimetypes import guess_type
+from werkzeug.utils import secure_filename
+from http import HTTPStatus
+from flask_jwt_extended import jwt_required, current_user
 
+FILE_UPLOAD_PATH = r"d:\projects\fitproject\instance\uploads"
 
 document_fields = {
-    'id': fields.String,
+    'id': fields.Integer,
     'name': fields.String,
     'type': fields.String(attribute="mime"),
     "ctime": fields.DateTime(attribute="uploaded")
 }
 
-documents_fields = {
-    "documents": fields.List(fields.Nested(document_fields))
-}
+parser = RequestParser()
+parser.add_argument(
+    'file', location='files', type=werkzeug.datastructures.FileStorage,
+    required=True)
 
 
 class DocumentResource(Resource):
-    @marshal_with(document_fields, envelope="documents")
+    @marshal_with(document_fields)
     def _get_documents(self):
         query = Select(Document)
         session = current_app.db.session
@@ -32,3 +44,29 @@ class DocumentResource(Resource):
         if id is None:
             return self._get_documents()
         return self._get_document(id)
+
+    @jwt_required()
+    @marshal_with(document_fields)
+    def post(self):
+        args = parser.parse_args()
+        file = args['file']
+        print(file.mimetype)
+        print(guess_type(file.filename))
+        mime = (file.mimetype or guess_type(file.filename)[0]
+                or "application/octet-stream")
+        filename = secure_filename(file.filename)
+        uuid = uuid4()
+        uploaded = datetime.now(tz=timezone.utc)
+        document = Document(
+            filename=str(uuid), name=filename, uploaded=uploaded, mime=mime,
+            owner=current_user)
+        session = current_app.db.session
+        try:
+            session.add(document)
+            file.save(os.path.join(FILE_UPLOAD_PATH, document.filename))
+            session.commit()
+        except (IntegrityError, FileExistsError, FileNotFoundError) as e:
+            session.rollback()
+            abort(HTTPStatus.INTERNAL_SERVER_ERROR, str(e))
+        else:
+            return document
