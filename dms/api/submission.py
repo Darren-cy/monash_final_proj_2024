a7807d7f7d9f8ff1b748143d0fbc0f6ae8d53815
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-05-04 18:28:03 +1000

    Added submission get endpoints

--- dms/api/submission.py
+++ dms/api/submission.py
@@ -1,9 +1,55 @@
 from flask_restful import Resource  # type: ignore
+from marshmallow import Schema, fields
+from sqlalchemy import select
+
+from dms import db
+from dms.models import Submission
+
+from .assessment import (AssessmentSchema, CriterionSchema, DocumentSchema,
+                         UserSchema)
+
+
+class AuthorSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String()
+
+
+class ResultSchema(Schema):
+    value = fields.Integer()
+    marker = fields.Nested(UserSchema, dump_only=True)
+    marked = fields.DateTime(dump_only=True)
+    criterion = fields.Nested(CriterionSchema(only=["name", "min", "max"]))
+
+
+class SubmissionSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    ctime = fields.DateTime(dump_only=True, attribute="submitted")
+    assessment_id = fields.Integer(load_only=True, data_key="assessment")
+    assessment = fields.Nested(AssessmentSchema(
+        only=["id", "name", "minMarks", "maxMarks"]))
+    totalMarks = fields.Integer(dump_only=True)
+    attachments = fields.Nested(DocumentSchema(many=True))
+    authors = fields.Nested(AuthorSchema(many=True))
+    results = fields.Nested(ResultSchema(many=True, only=["criterion", "value"]))
+
+
+submission_schema = SubmissionSchema()
+submissions_schema = SubmissionSchema(many=True)
 
 
 class SubmissionResource(Resource):
+    def _get_submission(self, id):
+        return submission_schema.dump(db.get_or_404(Submission, id))
+
+    def _get_submissions(self):
+        query = select(Submission)
+        submissions = db.session.scalars(query)
+        return submissions_schema.dump(submissions)
+
     def get(self, id=None):
-        raise NotImplementedError
+        if id is None:
+            return self._get_submissions()
+        return self._get_submission(id)
 
     def post(self):
         raise NotImplementedError
