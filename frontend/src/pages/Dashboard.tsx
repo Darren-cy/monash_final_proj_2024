b3c3d5d3ca7273083f8afd25b67f2a5d24c9c71b
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-05-10 00:46:43 +1000

    Not finished Dashboard need to fix the pop up

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
+import PopUpPanel from "../components/PopUpPanel";
+import { Data } from "../components/Schemas";
 
 const Dashboard = () => {
-  const [assessments] = useState([
-    { id: 1, fileName: "30221_euia", assessmentName: "Assessment 1", date: "2022-01-01", status: "Completed" },
-    { id: 25, fileName: "23131_jknakds", assessmentName: "Assessment 2", date: "2022-02-01", status: "Pending" },
-    { id: 3, fileName: "2134_sadgg", assessmentName: "Assessment 3", date: "2022-03-01", status: "In Review" },
-  ]);
-  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+  const [isOpen, setIsOpen] = useState(false);
+  const [assessments, setAssessment] = useState<Data[]>([]);
+
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: "ascending",
+  });
+
+  const backendUrl = import.meta.env.VITE_BACKEND_API_URL;
+  useEffect(() => {
+    // Fetch assessment from the API
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${backendUrl}/assessment`);
+        const jsonData = await response.json();
+        setAssessment(jsonData);
+        console.log(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const sortedAssessments = React.useMemo(() => {
     const sortableItems = [...assessments];
     if (sortConfig !== null) {
       sortableItems.sort((a, b) => {
-        if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? -1 : 1;
+        if (sortConfig.key !== null && a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
         }
-        if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? 1 : -1;
+        if (sortConfig.key !== null && a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
         }
         return 0;
       });
@@ -26,18 +46,23 @@ const Dashboard = () => {
     return sortableItems;
   }, [assessments, sortConfig]);
 
-  const requestSort = key => {
-    let direction = 'ascending';
-    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
-      direction = 'descending';
-    }
-    setSortConfig({ key, direction });
+  const SortIcon = ({ columnName }: { columnName: string }) => {
+    if (sortConfig.key !== columnName) return null;
+    return sortConfig.direction === "ascending" ? "↑" : "↓";
   };
 
-  const SortIcon = ({ columnName }) => {
-    if (sortConfig.key !== columnName) return null;
-    return sortConfig.direction === 'ascending' ? '↑' : '↓';
-  };
+  function requestSort(arg0: string): void {
+    throw new Error("Function not implemented.");
+  }
+
+  const handleClick = () => {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+    else {
+      setIsOpen(true);
+    }
+  }
 
   return (
     <div className="bg-gradient-to-r from-cyan-300 to-blue-200 min-h-screen">
@@ -48,26 +73,73 @@ const Dashboard = () => {
           <table className="min-w-full leading-normal border-collapse border border-gray-300">
             <thead>
               <tr>
-                <th className="border-b border-gray-300 cursor-pointer p-2 text-left" onClick={() => requestSort('id')}>ID <SortIcon columnName="id" /></th>
-                <th className="border-b border-gray-300 cursor-pointer p-2 text-left" onClick={() => requestSort('fileName')}>File Name <SortIcon columnName="fileName" /></th>
-                <th className="border-b border-gray-300 cursor-pointer p-2 text-left" onClick={() => requestSort('assessmentName')}>Assessment Name <SortIcon columnName="assessmentName" /></th>
-                <th className="border-b border-gray-300 cursor-pointer p-2 text-left" onClick={() => requestSort('date')}>Date <SortIcon columnName="date" /></th>
-                <th className="border-b border-gray-300 cursor-pointer p-2 text-left" onClick={() => requestSort('status')}>Status <SortIcon columnName="status" /></th>
-                <th className="border-b border-gray-300 p-2 text-left">Actions</th>
+                <th
+                  className="border-b border-gray-300 cursor-pointer p-2 text-left"
+                  onClick={() => requestSort("assessment_id")}
+                >
+                  Assessment ID <SortIcon columnName="assessment_id" />
+                </th>
+                <th
+                  className="border-b border-gray-300 cursor-pointer p-2 text-left"
+                  onClick={() => requestSort("assessmentName")}
+                >
+                  Assessment Name <SortIcon columnName="assessmentName" />
+                </th>
+                <th
+                  className="border-b border-gray-300 cursor-pointer p-2 text-left"
+                  onClick={() => requestSort("fileName")}
+                >
+                  File Name <SortIcon columnName="fileName" />
+                </th>
+                <th
+                  className="border-b border-gray-300 cursor-pointer p-2 text-left"
+                  onClick={() => requestSort("date")}
+                >
+                  Created on <SortIcon columnName="date" />
+                </th>
+                <th
+                  className="border-b border-gray-300 cursor-pointer p-2 text-left"
+                  onClick={() => requestSort("no_submission")}
+                >
+                  No Submission <SortIcon columnName="no_submission" />
+                </th>
+                <th className="border-b border-gray-300 p-2 text-left">
+                  Actions
+                </th>
               </tr>
             </thead>
             <tbody>
               {sortedAssessments.map((assessment) => (
                 <tr key={assessment.id} className="hover:bg-gray-100">
-                  <td className="border-b border-gray-300 p-2">{assessment.id}</td>
-                  <td className="border-b border-gray-300 p-2">{assessment.fileName}</td>
-                  <td className="border-b border-gray-300 p-2">{assessment.assessmentName}</td>
-                  <td className="border-b border-gray-300 p-2">{assessment.date}</td>
-                  <td className="border-b border-gray-300 p-2">{assessment.status}</td>
                   <td className="border-b border-gray-300 p-2">
-                    <Link to={`/document-processing`} className="text-blue-500 hover:text-blue-600">
+                    {assessment.id}
+                  </td>
+                  <td className="border-b border-gray-300 p-2">
+                    {assessment.name}
+                  </td>
+                  <td className="border-b border-gray-300 p-2">
+                    {assessment.rubric.name}
+                  </td>
+                  <td className="border-b border-gray-300 p-2">
+                    {Date(assessment.ctime).toString()}
+                  </td>
+                  <td className="border-b border-gray-300 p-2">
+                    {assessment.submissions.length}
+                  </td>
+                  <td className="border-b border-gray-300 p-2">
+                    {/* <Link
+                      to={`/document-processing/`}
+                      className="text-blue-500 hover:text-blue-600"
+                    >
                       View
-                    </Link>
+                    </Link> */}
+                    <button
+                      onClick={handleClick}
+                      className="text-blue-500 hover:text-blue-600"
+                    >
+                      View
+                    </button>
+                    {isOpen && <PopUpPanel isOpen={isOpen} items={assessment.submissions} />}
                   </td>
                 </tr>
               ))}
