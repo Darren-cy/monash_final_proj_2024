1c3e75dfe98ca58f93831f16a274bd7050123189
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-05-10 16:32:35 +1000

    Remove upload function in filedisplay component

--- frontend/src/components/FileDisplay.tsx
+++ frontend/src/components/FileDisplay.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 const FileDisplay = ({ fileUrl, side }: Props) => {
-  const [file, setFile] = useState<any | null>(null);
   const [fileType, setFileType] = useState<string>("");
   const backendUrl = import.meta.env.VITE_API_URL;
 
@@ -17,7 +16,6 @@ const FileDisplay = ({ fileUrl, side }: Props) => {
     const fetchFile = async () => {
       try {
         const response = await api.get(fileUrl);
-        setFile(response.data);
         setFileType(response.headers["content-type"]);
       } catch (error) {
         console.error(error);
@@ -26,29 +24,13 @@ const FileDisplay = ({ fileUrl, side }: Props) => {
     fetchFile();
   }, [fileUrl]);
 
-  const handleUpload = (event: any) => {
-    const uploadedFile = event.files[0];
-    const formData = new FormData();
-    formData.append("file", uploadedFile, uploadedFile.name);
-    api
-      .post(`${backendUrl}/api/v1.0/upload`, formData)
-      .then((response) => {
-        setFile(response.data);
-        setFileType(response.headers["content-type"]);
-        window.location.reload();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
   return (
     <div
       className={`p-1 w-1/2 h-500 inline-block align-top outline-black outline-offset-2 outline-1  ${
         side === "left" ? "pr-1" : "pl-1"
       }`}
     >
-      {file ? (
+      {(
         <div>
           {fileType.includes("video") ? (
             <div className="w-full h-full overflow-auto resize self-center" >
@@ -69,24 +51,6 @@ const FileDisplay = ({ fileUrl, side }: Props) => {
               </a>
             </div>
           )}
-        </div>
-      ) : (
-        <div>
-          <div className="card h-full justify-center flex items-center">
-            <FileUpload
-              name="file"
-              url={`${backendUrl}/api/v1.0/upload`}
-              onUpload={handleUpload}
-              accept="image/*,application/pdf,text/*,video/*,audio/*"
-              maxFileSize={1024 * 1024 * 10} // 10MB
-              emptyTemplate={
-                <div>
-                <p className="m-0">No file selected.</p>
-                <p className="m-0">Drag and drop files to here to upload.</p>
-                </div>
-              }
-            />
-          </div>
         </div>
       )}
     </div>
