fbac5e5f05578333b22ab2c97bc460e180cfea9a
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-05-10 18:29:51 +1000

    Update processing page

--- frontend/src/pages/DocumentProcessing.tsx
+++ frontend/src/pages/DocumentProcessing.tsx
@@ -5,24 +5,29 @@ import NavigationBar from "../components/NavigationBar";
 import { ResizableBox } from "react-resizable";
 import { useLocation } from "react-router-dom";
 import "react-resizable/css/styles.css";
+import api from "../api";
+import {
+  MarksCreate,
+  ResultCreate,
+  CriterionRead,
+} from "../components/Schemas";
 
 const DocumentProcessing = () => {
-  const [doc, setDoc] = useState("");
-  const [mark, setMark] = useState("");
-  const editorRef = useRef(null as any);
   const apikey = import.meta.env.VITE_TINYMCE_API_KEY;
   const backendUrlApi = import.meta.env.VITE_BACKEND_API_URL;
   const location = useLocation();
   const assessmentId = location.pathname.split("/")[2];
   const submissionId = location.pathname.split("/")[3];
+  const editorRef = useRef(null as any);
+  const [doc, setDoc] = useState("");
+  const [mark, setMark] = useState("");
   const [fileID, setFileID] = useState<any>(null);
   const [rubricID, setRubricID] = useState<any>(null);
+  const [criterias, setCriterias] = useState<CriterionRead[]>([]);
 
   useEffect(() => {
     fetchFileID(false);
     fetchFileID(true);
-    console.log(fileID);
-    console.log(rubricID);
   }, []);
 
   const fetchFileID = async (isRubric: boolean) => {
@@ -33,6 +38,7 @@ const DocumentProcessing = () => {
         );
         const jsonAssessment = await response.json();
         setRubricID(jsonAssessment.rubric.id);
+        setCriterias(jsonAssessment.criteria);
       } else {
         const response = await fetch(
           `${backendUrlApi}/submission/${submissionId}`
@@ -40,6 +46,33 @@ const DocumentProcessing = () => {
         const jsonData = await response.json();
         setFileID(jsonData.attachments[0].id);
       }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const fetchResults = async () => {
+    try {
+      const response = await api.get(
+        `${backendUrlApi}/submission/${submissionId}/mark`
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const marks: MarksCreate = {
+        results: [],
+        feedback: doc,
+      };
+      const response = await api.post(
+        `${backendUrlApi}/submission/${submissionId}/mark`,
+        marks
+      );
+      console.log(response);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -56,40 +89,22 @@ const DocumentProcessing = () => {
     <div className="bg-gradient-to-r from-cyan-300 to-blue-200 min-h-screen">
       <NavigationBar />
       <div className="p-6 text-black flex flex-col h-full">
-        <ResizableBox
-          className="flex-grow"
-          height={300}
-          width={Infinity}
-          axis="y"
-          resizeHandles={["s"]}
-        >
-          <div className="flex h-full">
-            <ResizableBox
-              className="w-1/2"
-              width={200}
-              height={Infinity}
-              axis="x"
-              resizeHandles={["e"]}
-            >
-              <div className="flex flex-col h-full pr-2">
-                <h2 className="text-lg font-semibold mb-2">
-                  Student Submission
-                </h2>
-                <FileDisplay
-                  fileUrl={`${backendUrlApi}/document/${fileID}/download`}
-                  side="left"
-                />
-              </div>
-            </ResizableBox>
-            <div className="flex-1 pl-2">
-              <h2 className="text-lg font-semibold mb-2">Rubrics</h2>
-              <FileDisplay
-                fileUrl={`${backendUrlApi}/document/${rubricID}/download`}
-                side="right"
-              />
-            </div>
+        <div className="flex h-500">
+          <div className="flex-1 pl-2 w-full">
+            <h2 className="text-lg font-semibold mb-2">Student Submission</h2>
+            <FileDisplay
+              fileUrl={`${backendUrlApi}/document/${fileID}/download`}
+              side="left"
+            />
           </div>
-        </ResizableBox>
+          <div className="flex-1 pr-2 w-full">
+            <h2 className="text-lg font-semibold mb-2">Rubrics</h2>
+            <FileDisplay
+              fileUrl={`${backendUrlApi}/document/${rubricID}/download`}
+              side="right"
+            />
+          </div>
+        </div>
         <div className="mt-2">
           <h2 className="text-lg font-semibold mb-2">Feedback and Marking</h2>
           <Editor
@@ -113,14 +128,21 @@ const DocumentProcessing = () => {
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
           />
-          <div className="mt-2 flex justify-between items-center">
-            <input
-              type="text"
-              placeholder="Enter mark"
-              value={mark}
-              onChange={(e) => setMark(e.target.value)}
-              className="border p-1 rounded"
-            />
+          {criterias.map((criteria) => (
+            <div key={criteria.id}>
+              <h2 className="text-lg font-semibold mb-2">{criteria.name}</h2>
+              <p>
+                {criteria.minMarks} - {criteria.maxMarks}
+              </p>
+              <input
+                type="number"
+                placeholder="Enter mark"
+                className="border border-gray-300 rounded p-2 w-1/4"
+                onChange={(e) => setMark(e.target.value)}
+              />
+            </div>
+          ))}
+          <div className="mt-2">
             <button
               onClick={log}
               className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
