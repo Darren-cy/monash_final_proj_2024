0899f85a3373e0576694c484272371ecca64bc10
Cong Chuong Truong <ctru0009@student.monash.edu>
2024-05-11 00:15:21 +1000

    Update Schemas.tsx to add new schemas

--- frontend/src/components/Schemas.tsx
+++ frontend/src/components/Schemas.tsx
@@ -1,49 +1,82 @@
 interface Attachment {
-    id: number;
-    name: string;
-  }
-  
-  interface Submission {
-    id: number;
-    ctime: string;
-    totalMarks: number;
-    attachments: Attachment[];
-    authors: Owner[];
-    results: any[];
-    feedback: any | null;
-  }
-  
-  interface Owner {
-    id: number;
-    name: string;
-  }
-  
-  interface Data {
-    id: number;
-    name: string;
-    ctime: string;
-    rubric: Attachment;
-    owner: Owner;
-    minMarks: number;
-    maxMarks: number;
-    submissions: Submission[];
-  }
-interface ResultCreate {
-    value: number;
-    criterion: number;
-    comments: string | null;
+  id: number;
+  name: string;
 }
 
-  interface MarksCreate{
-    results: ResultCreate[];
-    feedback: string | null;
-  }
+interface Submission {
+  id: number;
+  ctime: string;
+  totalMarks: number;
+  attachments: Attachment[];
+  authors: User[];
+  results: any[];
+  feedback: any | null;
+}
 
-  interface CriterionRead {
-    id: number;
-    name: string;
-    minMarks: number;
-    maxMarks: number;
-  }
+interface User {
+  id: number;
+  name: string;
+}
 
-  export type { Attachment, Submission, Owner, Data, ResultCreate, MarksCreate, CriterionRead };
+interface Data {
+  id: number;
+  name: string;
+  ctime: string;
+  rubric: Attachment;
+  owner: User;
+  minMarks: number;
+  maxMarks: number;
+  submissions: Submission[];
+}
+interface ResultCreate {
+  value: number;
+  criterion: number;
+}
+
+interface MarksCreate {
+  results: ResultCreate[];
+  feedback: string | null;
+}
+
+interface CriterionRead {
+  id: number;
+  name: string;
+  minMarks: number;
+  maxMarks: number;
+}
+
+interface CriterionCreate {
+  name: string;
+  min: number;
+  max: number;
+}
+
+interface Document {
+  id: number;
+  name: string;
+  type: string;
+  ctime: string;
+  size: number;
+  owner: User;
+  downloadURL: string;
+}
+
+interface Criteria {
+  name: string;
+  min: number;
+  max: number;
+  assessment_id: number;
+}
+
+export type {
+  Attachment,
+  Submission,
+  User as Owner,
+  Data,
+  ResultCreate,
+  MarksCreate,
+  CriterionRead,
+  Document,
+  Criteria,
+  CriterionCreate,
+};
