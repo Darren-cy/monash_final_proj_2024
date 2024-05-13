2bda172d771b53f6a31f952ad37bfe3a38d42d3f
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-05-13 19:21:03 +1000

    Added test stubs for session

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,2 +1,14 @@
-def test_session(app):
-    assert True
+def test_login_valid_credentials(app):
+    raise NotImplementedError
+
+
+def test_login_invalid_credentials(app):
+    raise NotImplementedError
+
+
+def test_logout_logged_in(app):
+    raise NotImplementedError
+
+
+def test_logout_not_logged_in(app):
+    raise NotImplementedError
