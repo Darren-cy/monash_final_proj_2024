947edc516abc5ece401f6c1c1206f6166637d725
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-05-13 19:05:49 +1000

    Updated app fixture and started implementation of session tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,31 @@
+import os
+import tempfile
+
+import flask_migrate
 import pytest
+from sqlalchemy import URL
 
 from dms import create_app
 
 
 @pytest.fixture
 def app():
+    db_fp, db_path = tempfile.mkstemp()
+    db_url = URL.create("sqlite", database=db_path)
+    print(str(db_url))
+
     app = create_app({
         "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": str(db_url),
     })
-    return app
+
+    with app.app_context():
+        flask_migrate.upgrade()
+
+    yield app
+
+    os.close(db_fp)
+    # os.remove(db_path)
 
 
 @pytest.fixture
