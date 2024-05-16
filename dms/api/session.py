bec8763935171cfa5247883345031491b8130d46
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-05-16 19:37:33 +1000

    Updated password hashing and checks happen in User objects

--- dms/api/session.py
+++ dms/api/session.py
@@ -1,17 +1,15 @@
 from http import HTTPStatus
-from werkzeug.security import check_password_hash
 
+from email_validator import EmailNotValidError, validate_email
 from flask import abort, jsonify
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 from flask_restful import Resource  # type: ignore
 from flask_restful.reqparse import RequestParser  # type: ignore
 from sqlalchemy import select
 from sqlalchemy.exc import NoResultFound
-from email_validator import EmailNotValidError, validate_email
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 
+from dms import db, jwt_blocklist
 from dms.models import User
-from dms import jwt_blocklist, db
-
 
 parser = RequestParser()
 parser.add_argument("email", required=True)
@@ -37,11 +35,11 @@ class SessionResource(Resource):
         statement = select(User).where(User.email == email)
         dbsession = db.session
         try:
-            user = dbsession.scalars(statement).one()
+            user: User = dbsession.scalars(statement).one()
         except NoResultFound:
             abort_on_invalid_credentials()
 
-        if not check_password_hash(user.password, password):
+        if not user.check_password_hash(password):
             abort_on_invalid_credentials()
 
         access_token = create_access_token(identity=user)
