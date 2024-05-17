7ccbce075cf12e333cfd3642ab602aa50a9ecdbe
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.comclear>
2024-05-17 14:48:39 +1000

    Added test stubs

---
+++ tests/test_assessment.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.xfail
+def test_get_assessments(client):
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_get_assessment(client):
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_create_assessment(client):
+    raise NotImplementedError
---
+++ tests/test_document.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.xfail
+def test_get_documents(client):
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_get_document(client):
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_download_document(client):
+    raise NotImplementedError
+
+
+@pytest.mark.xfail
+def test_upload_document(client):
+    raise NotImplementedError
