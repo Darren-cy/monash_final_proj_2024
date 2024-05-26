f7a10890efe28c2d0c76167c7310e58440d7aa07
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2024-05-26 18:03:55 +1000

    added document retrieval test

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,9 +1,40 @@
 import pytest
+from http import HTTPStatus
+
+DOCUMENTS = [{"id": 1,
+              "name": "Rubric1.pdf",
+              "type": "application/pdf",
+            #   "ctime": "2024-04-05T09:30:00",
+              "size": 10240,
+              "owner": {"id": 1,
+                        "name": "Test User"},
+              "downloadURL": "/api/v1.0/document/1/download"},
+             {"id": 2,
+              "name": "Rubric2.pdf",
+              "type": "application/pdf",
+            #   "ctime": "2024-05-01T09:30:00",
+              "size": 11568,
+              "owner": {"id": 2,
+                        "name": "Alice Wang"},
+              "downloadURL": "/api/v1.0/document/2/download"},
+             {"id": 3,
+              "name": "submission1.pdf",
+              "type": "application/pdf",
+            #   "ctime": "2024-05-10T17:45:36",
+              "size": 76864,
+              "owner": {"id": 3,
+                        "name": "Bob Smith"},
+              "downloadURL": "/api/v1.0/document/3/download"}]
 
 
-@pytest.mark.xfail
 def test_get_documents(client):
-    raise NotImplementedError
+    response = client.get("/api/v1.0/document")
+    assert response.status_code == HTTPStatus.OK
+    j = response.json
+    del j[0]["ctime"]
+    del j[1]["ctime"]
+    del j[2]["ctime"]
+    assert j== DOCUMENTS
 
 
 @pytest.mark.xfail
